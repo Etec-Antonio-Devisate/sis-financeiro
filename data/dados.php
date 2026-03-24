c603de05b59d6385d28bdcae451b3787cec8dfcf
Aluno <aluno@devisate.local>
2026-03-24 10:55:52 -0300

    introdução ao sistema financeiro

---
+++ data/dados.php
@@ -0,0 +1,23 @@
+<?php
+$usuarios = [];
+$contas = [];
+
+$usuarios = [
+	[
+    	"usuario" => "cliente",
+    	"senha" => "123",
+    	"perfil" => "cliente"
+	],
+	[
+    	"usuario" => "empresa",
+    	"senha" => "123",
+    	"perfil" => "empresa"
+	],
+	[
+    	"usuario" => "admin",
+    	"senha" => "123",
+    	"perfil" => "proprietario"
+	]
+];
+?>
+
